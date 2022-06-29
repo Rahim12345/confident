69df94363afe3eb8459c196e5488dad8cd995e5f
Rahim Suleymanov <rahim.suleymanov@adpu.edu>
2022-06-29 15:30:53 +0400

    login is ready

---
+++ app/Http/Requests/StoreOptionRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreOptionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'facebook'=>'required|max:255',
+            'twitter'=>'required|max:255',
+            'instagram'=>'required|max:255',
+            'youtube'=>'required|max:255',
+            'email'=>'required|max:255',
+            'tel'=>'required|max:255',
+            
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'facebook'=>'Facebook',
+            'twitter'=>'Twitter',
+            'instagram'=>'Instagram',
+            'youtube'=>'YouTube',
+            'email'=>'Email',
+            'tel'=>'Telefon',
+            
+        ];
+    }
+}
+
+
+        
